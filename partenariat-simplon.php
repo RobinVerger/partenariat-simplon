7de6e74bfaedd8d578bc7cdf4885c2339a917573
RobinV <32733389+RobinVerger@users.noreply.github.com>
2017-12-07 17:19:41 +0100

    Ajout ShortCode
    Ajout d'un ShortCode pour un plugin

--- partenariat-simplon.php
+++ partenariat-simplon.php
@@ -1,10 +1,15 @@
 <?php
 /*
-
+Plugin Name: simplon_plugin
+Plugin URI: 
+Description: Un plugin qui fait de la pub pour simplon
+Version: 0.1
+Author: Robin
 */
 
-
-
-
-
-?>
+function ShortcodeSimplon() {
+	return '<p>La publication de cet article est possible grâce à mon super partenaire simplon.co - une entreprise de
+    l’économie sociale et solidaire et un réseau de « fabriques » (écoles) qui propose
+    des formations GRATUITES pour devenir développeur web.</p>';
+}
+add_shortcode('simplon', 'ShortcodeSimplon');
